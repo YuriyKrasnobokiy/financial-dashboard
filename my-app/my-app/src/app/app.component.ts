87f1c48b16fbf7a0d554668d195714a1deea0c2c
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-02-03 23:38:02 +0200

    added get request

--- my-app/my-app/src/app/app.component.ts
+++ my-app/my-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  users: string[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getUsers().subscribe((data: any[]) => {
+      // Вибираємо значення ключа "user" та мапимо їх до масиву users
+      this.users = data.map((item: any) => item.user);
+    });
+  }
 }
